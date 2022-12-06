06999b48f4b94aac5d9c4a1bacb36d204836bcba
costanza <rodric@usi.ch>
2022-12-06 19:44:37 +0100

    fatto un po' di cose tutte insieme

--- Code.rkt
+++ Code.rkt
@@ -1,6 +1,5 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname Code) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#lang htdp/asl
+
 (require 2htdp/universe)
 (require 2htdp/image)
 (require racket/vector)
