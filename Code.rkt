a0138057a230932175e906c58e4b77ccf9738c4e
costanza <rodric@usi.ch>
2022-12-08 17:41:20 +0100

    loser function

--- Code.rkt
+++ Code.rkt
@@ -503,20 +503,11 @@
 
 ; LOSER FUNCTION
 ; takes a World-state and checks if the us
-<<<<<<< Updated upstream
-#|
-* se la riga 20 non ha solo blocchi vuoti: hai perso
-     * non spawnano piu pezzi
-     * ti esce un messaggio
-     * press key to restart (forse)
-=======
 ; * se la riga 20 non ha solo blocchi vuoti: hai perso
 ; * non spawnano piu pezzi
 ; * ti esce un messaggio
 ; * press key to restart (forse)
->>>>>>> Stashed changes
 
-|#
 
 ; HANDLE-KEY FUNCTION
 ;
