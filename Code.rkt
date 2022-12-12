00886e7ffcb8e0cbf0c3ad0eee8f779e7c1b8c3d
costanza <rodric@usi.ch>
2022-12-12 17:42:27 +0100

    updated to do

--- Code.rkt
+++ Code.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname Code) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;#lang htdp/asl
 
 (require 2htdp/universe)
@@ -863,14 +860,10 @@
 
 ;;; TO DO:
 
-
-;;; * any-full-rows
 ;;; * handle-key: 
 ;;;   rotate 
 ;;; * la situa dello score 
-;;; * quit
 ;;; * fare template e check-expect
-;;; * cambiare colore dello score
 
 
 ;;; * README
